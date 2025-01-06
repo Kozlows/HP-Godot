a81020b39d84cfbf46e1b6a14387e864f89c4d6f
Kozlows <szymon.p.kozlowski@gmail.com>
2025-01-06 14:20:28 +0000

    Added template for keybinds functionality and certain detection checks in settings menu

---
+++ scripts/keybinds.gd
@@ -0,0 +1,19 @@
+class_name Keybinds
+extends Resource
+
+
+const left : String = "move_left"
+
+@export var DEFAULT_MOVE_LEFT_KEY = InputEventKey.new()
+
+var move_left_key = InputEventKey.new()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,11 +1,39 @@
 extends Control
 
+@onready var keybinds : Keybinds = preload("res://scripts/keybinds.tres")
+
+const configPath : String = "user://config.cfg"
+
+func load_config() -> void:
+	var config = ConfigFile.new()
+	
+	if config.load(configPath) != OK:
+		print("Something went wrong when loading file")
+	elif not config.get_sections():
+		print("File is empty")
+		config = default_config(config)  # set config to default values
+	else:
+		print("Succeeded in loading file")
+	
+	print(config.get_sections())
+	setup_settings(config)
+
+
+func default_config(configFile : ConfigFile) -> ConfigFile:
+	# configure default options here
+	configFile.set_value("Test", "testKey", "testValue")
+	configFile.save(configPath)
+	return configFile
+	
+
+func setup_settings(configFile : ConfigFile) -> void:
+	# get all the values from the config, and display them under the
+	# corresponding tabs
+	pass
+
 func _ready() -> void:
-	$MarginContainer/Options/Volume.grab_focus()
-
-
-func _on_volume_pressed() -> void:
-	pass # Replace with function body.
+	load_config()
+	$MarginContainer/Options/Back.grab_focus()
 
 
 
